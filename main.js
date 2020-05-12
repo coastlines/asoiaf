ff46c3e6ee59852ba0e68fdda1daf39b968b136d
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-12 00:11:27 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,19 +1,68 @@
 'use strict';
 
+let isAlive = {}
+let isDead = {}
+
 window.onload = function() {
-  getHouse();
+  getAlive()
+  getDead()
   console.log("hello!")
 }
 
-const getHouse = () => {
-  fetch('https://www.anapioficeandfire.com/api/houses')
+const getAlive = () => {
+  fetch('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=2&isAlive=true')
     .then(response => { 
       console.log("Fetch is done! response =", response)
       return response.json()
     })
-    .then(data => {
-      console.log("Got my data! data =", data.length)
-      data.forEach(updateHtml);
+    .then(alive => {
+      console.log("Got my data! alive=", alive.length)
+      //data.forEach(updateHtml);
+      alive.forEach(quiz);
+      alive => isAlive = alive
     })
 }
 
+const getDead = () => {
+  fetch('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=2&isAlive=false')
+    .then(response => { 
+      console.log("Fetch is done! response =", response)
+      return response.json()
+    })
+    .then(dead => {
+      console.log("Got my data! dead =", dead.length)
+      dead.forEach(quiz);
+      dead => isDead = dead
+    })
+}
+
+
+let quiz = function(quizInput) {
+  console.log("Adding quiz html for ", quizInput);
+
+}
+
+
+// let updateHtml = function(house) {
+//   console.log("Updating the html for post = ", house);
+//   let postsUl = document.getElementById("posts");
+
+//   let postLi = document.createElement("li");
+//   postLi.innerText = house.coatOfArms;
+//   postsUl.appendChild(postLi);
+
+//   let name = house.name;
+//   let userSpan = document.createElement("span");
+//   userSpan.innerText = "    -  by"+ name;
+//   postLi.appendChild(userSpan);
+
+//   fetch("https://www.anapioficeandfire.com/api/houses"+name)
+//   .then(function(response) {
+//     return response.json()
+//   })
+//   .then(function(data) {
+//     gotHouse[name] = data;
+//     userSpan.innerText = "    -  by"+ data.name;
+//   })
+  
+// }
