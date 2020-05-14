cbc2e997019a8fb03c36dcf8dc611907e6a27b0d
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-14 01:13:48 -0500

    html, css, js, hide, display, select

--- main.js
+++ main.js
@@ -1,68 +1,172 @@
 'use strict';
 
-let isAlive = {}
-let isDead = {}
-
 window.onload = function() {
-  getAlive()
-  getDead()
-  console.log("hello!")
+  getHouse()
 }
 
-const getAlive = () => {
-  fetch('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=2&isAlive=true')
+const getHouse = () => {
+ // the promise 
+      fetch('https://www.anapioficeandfire.com/api/houses?pageSize=8&hasWords=true')
+ //  with the promise, we want to do this 
     .then(response => { 
-      console.log("Fetch is done! response =", response)
-      return response.json()
+      return response.json()  // this is also a promise
     })
-    .then(alive => {
-      console.log("Got my data! alive=", alive.length)
-      //data.forEach(updateHtml);
-      alive.forEach(quiz);
-      alive => isAlive = alive
-    })
-}
-
-const getDead = () => {
-  fetch('https://www.anapioficeandfire.com/api/characters?page=1&pageSize=2&isAlive=false')
-    .then(response => { 
-      console.log("Fetch is done! response =", response)
-      return response.json()
-    })
-    .then(dead => {
-      console.log("Got my data! dead =", dead.length)
-      dead.forEach(quiz);
-      dead => isDead = dead
+    .then(house => {
+      house.forEach(nameDiv);
+      house.forEach(wordsDiv);
     })
 }
 
 
-let quiz = function(quizInput) {
-  console.log("Adding quiz html for ", quizInput);
+let nameDiv= function(house) {
+  let section = document.getElementById("main");
 
-}
+  let div = document.createElement("div");
+  let p = document.createElement("p");
 
-
-// let updateHtml = function(house) {
-//   console.log("Updating the html for post = ", house);
-//   let postsUl = document.getElementById("posts");
-
-//   let postLi = document.createElement("li");
-//   postLi.innerText = house.coatOfArms;
-//   postsUl.appendChild(postLi);
-
-//   let name = house.name;
-//   let userSpan = document.createElement("span");
-//   userSpan.innerText = "    -  by"+ name;
-//   postLi.appendChild(userSpan);
-
-//   fetch("https://www.anapioficeandfire.com/api/houses"+name)
-//   .then(function(response) {
-//     return response.json()
-//   })
-//   .then(function(data) {
-//     gotHouse[name] = data;
-//     userSpan.innerText = "    -  by"+ data.name;
-//   })
+  div.className = 'name';
+  p.className = 'hidden';
+  p.innerHTML = house.name;
   
+  div.appendChild(p);
+  section.appendChild(div);
+}
+
+
+let wordsDiv= function(house) {
+  let section = document.getElementById("main");
+ 
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+
+  div.className = 'words';
+  p.className = 'hidden';
+  p.innerHTML = house.words;
+
+  div.appendChild(p);
+  section.appendChild(div);
+}
+
+
+const randomize = () => {
+  let div = document.querySelectorAll('div');
+  let p = document.querySelectorAll('p');
+  let divArray = Array.from(div);
+  let pArray = Array.from(p);
+
+  for (let i = 0; i < divArray.length; i++) {
+    let target = Math.floor(Math.random() * divArray.length -1) + 1;
+    let target2 = Math.floor(Math.random() * divArray.length -1) +1;
+    divArray[target].before(divArray[target2]);
+    pArray[i].classList.remove('hidden');
+  }
+
+  selectDiv(divArray)
+}
+
+const selectDiv = (divs) => {
+let selectCount = 0;
+let div =  document.querySelectorAll('div');
+let select =  document.querySelectorAll('.select');
+let divArray = Array.from(div);
+  
+for (let i = 0; i < divArray.length; i++) {
+  if ( selectCount < 2) {
+    divArray[i].addEventListener('click', () => {
+      console.log("spread add forEach worked");
+      divArray[i].classList.add('select');
+    })
+  }
+}
+}
+//unSelectDiv(div)
+
+
+
+
+
+// let div = [...divs].forEach((div) => {
+  
+//   if (selectCount < 0) {
+
+//     div.addEventListener('click', () => {
+//       console.log("spread add forEach worked");
+//       div.classList.add('select');
+//       selectCount++;
+//     })
+//   }
+
+
+// })
+
+// const unSelectDiv = (div) => {
+//   div.addEventListener('click', () => {
+//     console.log("spread add forEach worked");
+//     div.classList.remove('select');
+//   });
+//   }
+
+// const selectDiv = () => {
+// // cards array holds all cards
+// let card = document.getElementsByTagName('div');
+// let cards = [...card];
+// // loop to add event listeners to each card
+// for (var i = 0; i < cards.length; i++){
+//   cards[i].addEventListener("click", displayCard);
+//   console.log(card);
+// };
+
+// var displayCard = function (){
+//   this.classList.toggle("open");
+//   this.classList.toggle("show");
+//   this.classList.toggle("disabled");
+// };
+
 // }
+
+// // Fisher-Yates (aka Knuth) Shuffle
+// function shuffle(array) {
+//   var currentIndex = array.length, temporaryValue, randomIndex;
+
+//   while (currentIndex !== 0) {
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
+//   return array;
+// }
+
+
+// // deck of all cards in game
+// const deck = document.querySelector(".deck");
+// function startGame(){
+//    var shuffledCards = shuffle(cards);
+//    for (var i= 0; i < shuffledCards.length; i++){
+//       [].forEach.call(shuffledCards, function(item){
+//          deck.appendChild(item);
+//       });
+//    }
+// }
+
+//window.onload = startGame(cards);
+
+
+
+//displayCard is a function we'll talk about this soon
+
+  //let divArray = Array.from(div);
+
+//   var $cols = $('.divs').click(function(e) {
+//     $cols.removeClass(addclass);
+//     $(this).addClass(addclass);
+//   });
+// }
+
+
+//   document.getElementById("Save").onclick = function fun() {
+//       alert("hello");
+//       f1();
+//       //validation code to see State field is mandatory.  
+//   }
